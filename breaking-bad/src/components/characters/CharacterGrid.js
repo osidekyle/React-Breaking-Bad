8c4d510da803d0e4f988127b219c0b14a975edfc
osidekyle <hawkinskyle67@gmail.com>
2020-07-27 02:28:55 -0700

    create character grid, if loading put is loading, if not put list of characters

--- breaking-bad/src/components/characters/CharacterGrid.js
+++ breaking-bad/src/components/characters/CharacterGrid.js
@@ -1,7 +1,12 @@
 import React, { Component } from 'react';
 
-const CharacterGrid = () => {
-    return (<div></div>  );
+const CharacterGrid = (items, isLoading) => {
+    return isLoading ? (<h1>Loading...</h1>) :  <section className="cards">
+        {items.map(item => (
+            <h1 key={item.char_id}>{item.name}</h1>
+        ))}
+    </section>
+    
 }
  
 export default CharacterGrid;
