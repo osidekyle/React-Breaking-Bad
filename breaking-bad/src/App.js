cd456748893b8d532f16452751e1ae599b49f233
osidekyle <hawkinskyle67@gmail.com>
2020-07-27 23:27:57 -0700

    created search bar, passed query up to app.js and used to recall api with useEffect dependency

--- breaking-bad/src/App.js
+++ breaking-bad/src/App.js
@@ -2,6 +2,7 @@ import React, {useState, useEffect} from 'react';
 import Header from './components/ui/Header'
 import axios from 'axios'
 import CharacterGrid from './components/characters/CharacterGrid'
+import Search from './components/ui/Search'
 import './App.css';
 
 
@@ -11,18 +12,18 @@ import './App.css';
 const App =()=> {
   const [items, setItems]= useState([])
   const [isLoading, setIsLoading]= useState(true)
-
+  const [query, setQuery]= useState("")
 
   useEffect(()=>{
       const fetchItems = async () =>{
-          const result= await axios(`https://www.breakingbadapi.com/api/characters`);
+          const result= await axios(`https://www.breakingbadapi.com/api/characters?name=${query}`);
           setItems(result.data)
           setIsLoading(false)
           
       }
       fetchItems();
       console.log(items)
-  },[])
+  },[query])
 
 
 
@@ -31,6 +32,7 @@ const App =()=> {
   return (
     <div className="container">
       <Header/>
+      <Search getQuery={(q)=>setQuery(q)}/>
       <CharacterGrid isLoading={isLoading} items={items}/>
   
     </div>
--- breaking-bad/src/components/ui/Search.js
+++ breaking-bad/src/components/ui/Search.js
@@ -1,10 +1,24 @@
 import React, { useState } from 'react';
 
-const Search = () => {
+const Search = ({getQuery}) => {
+
+    const [text,setText]=useState("");
+
+    const onChange=(q)=>{
+        setText(q)
+        getQuery(q)
+    }
+
     return ( 
-        <div>
-            
-        </div>
+        <section className='search'>
+            <form>
+                <input type="text" className="form-control"
+                 placeholder="Search characters"
+                 value={text}
+                 onChange={(e)=>onChange(e.target.value)}
+                />
+            </form>
+        </section>
      );
 }
  
